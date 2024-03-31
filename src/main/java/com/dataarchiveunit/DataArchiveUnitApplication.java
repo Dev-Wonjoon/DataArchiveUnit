c8dce5bc81c2d2b7e63f502c069cf2a2f05a8979
dev-Wonjoon <dnjswns775@gmail.com>
2024-04-01 01:38:04 +0900

    Initial: 프로젝트 초기화

---
+++ src/main/java/com/dataarchiveunit/DataArchiveUnitApplication.java
@@ -0,0 +1,13 @@
+package com.dataarchiveunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataArchiveUnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataArchiveUnitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dataarchiveunit/DataArchiveUnitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataarchiveunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataArchiveUnitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
